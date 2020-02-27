45ebeab20b1f69018080bc359fc1ac12868c90b2
Viktor Eikman <viktor.eikman@gmail.com>
2020-02-27 21:35:14 +0100

    Added bolt length unit tests.

--- test/scad_tarmi/threaded_test.clj
+++ test/scad_tarmi/threaded_test.clj
@@ -50,3 +50,27 @@
       (is (= (flare 0 5) [0 4.001]))
       (is (= (flare 1 5) [2 4.001]))
       (is (= (flare 2 5) [2 4.001])))))
+
+(deftest length-predictor
+    "Testing the predictor of total bolt length."
+    (testing "Inadequate parameter set."
+      (is (thrown? java.lang.AssertionError
+            (threaded/total-bolt-length {:iso-size 3}))))
+    (testing "Total length only."
+      (is (= (threaded/total-bolt-length
+               {:iso-size 3, :total-length 1})
+             1)))
+    (testing "Head type only."
+      (is (= (threaded/total-bolt-length
+               {:iso-size 3, :head-type :countersunk})
+             3/2)))
+    (testing "Mutual redundance."
+      (is (= (threaded/total-bolt-length
+               {:iso-size 3, :total-length 1, :unthreaded-length 2,
+                :threaded-length 3, :head-type :countersunk})
+             1)))
+    (testing "Sum without explicit total length."
+      (is (= (threaded/total-bolt-length
+               {:iso-size 3, :unthreaded-length 2,
+                :threaded-length 3, :head-type :countersunk})
+             13/2))))
