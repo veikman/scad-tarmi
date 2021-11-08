1c4274aeac29b1c728991de6c3e9d592a531a762
Viktor Eikman <viktor.eikman@gmail.com>
2021-11-08 07:09:10 +0100

    Expanded unit tests.
    False negative for undesirable behaviour.

--- test/scad_tarmi/dfm_test.clj
+++ test/scad_tarmi/dfm_test.clj
@@ -4,17 +4,17 @@
 
 (deftest error-functions
   (testing "scalar/arithmetic use of error-fn."
-    (let [c (error-fn)]
+    (let [c (error-fn 10)]
       (is (= (c 0) 0))
       (is (= (c 0.0) 0))
-      (is (= (c -0.4) 0.09999999999999998))  ; Floating-point shenanigans.
-      (is (= (c -0.5) 0.0))
-      (is (= (c -0.6) -0.09999999999999998))
-      (is (= (c 0.5) 1.0))
-      (is (= (c 10) 10.5))
-      (is (= (c 10 {:negative true}) 10.5))
-      (is (= (c 10 {:negative false}) 9.75))
-      (is (= (c 10 {:negative false, :factor 3}) 9.25)))
+      (is (= (c 9) -1))
+      (is (= (c 10) 0))
+      (is (= (c 11) 1))
+      (is (= (c 12) 2))
+      (is (= (c 100) 90))
+      (is (= (c 100 {:negative true}) 90))
+      (is (= (c 100 {:negative false}) 105))
+      (is (= (c 100 {:negative false, :factor 3}) 115)))
     (let [c (error-fn 1)]
       (is (= (c 10) 9))
       (is (= (c 10 {:negative true}) 9))
@@ -25,7 +25,20 @@
            `(:scale [11/10 11/10 1] ::a))))
   (testing "vector/multiplicative use of error-fn for a positive shape."
     (is (= ((error-fn -1) 10 {:negative false} ::a)
-           `(:scale [19/20 19/20 1] ::a)))))
+           `(:scale [19/20 19/20 1] ::a))))
+  (testing "default error-fn."
+    (let [c (error-fn)]
+      (is (= (c -0.5) 0.0))
+      (is (= (c -0.4) 0.09999999999999998))
+      (is (= (c -0.3) 0.2))
+      (is (= (c -0.2) 0.3))
+      (is (= (c -0.1) 0.4))
+      (is (= (c 0.0) 0))
+      (is (= (c 0.1) 0.6))
+      (is (= (c 0.2) 0.7))
+      (is (= (c 0.3) 0.8))
+      (is (= (c 0.4) 0.9))
+      (is (= (c 0.5) 1.0)))))
 
 (deftest no-error
   (testing "the none function with default options."
